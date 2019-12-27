eb55262a62d396476ac71d6e08d8dddc3ba61e08
Yusuf <yusufnafey@yahoo.com>
2019-12-26 21:42:17 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [photo, setPhoto] = useState('')
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      initial
     </div>
   );
 }
